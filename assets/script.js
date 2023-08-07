9c931f0100bcd557660bc5ec17ac7efdc6db32d1
joceamayar <jocelin@tavlee.com>
2023-08-07 17:35:04 -0400

    display image from API, added button inside  image, commented add some code that was't allow me to work with the imae

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,25 @@
 let nasaBox = document.getElementById('nasa-images')
-let newNasaButton = document.getElementById('new-image')
+let nasaFixedImage = document.getElementById('nasa-img')
+let newNasaButton = document.getElementById('next-img-button')
 
-function getNasa() { 
+function getNasa() {
     // calls nasa api for an image of the day and makes new img element
-    fetch (`https://api.nasa.gov/planetary/apod?api_key=p027dcXDBzAB1YTjEeaiAA2djDcIYC5joRZl66GR&count=1`)
-    .then(response => response.json())
-    .then (data => {
-    
-        let nasaImg = document.createElement('img')
-        nasaImg.src = data[0].hdurl
-        nasaBox.append(nasaImg)
-    })
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=p027dcXDBzAB1YTjEeaiAA2djDcIYC5joRZl66GR&count=1`)
+        .then(response => response.json())
+        .then(data => {
+            // let nasaImg = document.createElement('img')
+            // nasaImg.src = data[0].hdurl
+            // nasaBox.append(nasaImg)
+
+            nasaFixedImage.src = data[0].hdurl;
+        })
 }
 
 // get new image when user clicks button
 newNasaButton.addEventListener('click', function (event) {
     event.preventDefault()
     getNasa();
-    nasaBox.textContent = ''
+    // nasaBox.textContent = ''
 })
 // commented load function out to save daily api calls
 // getNasa();
@@ -25,24 +27,24 @@ newNasaButton.addEventListener('click', function (event) {
 let nextQuoteBtn = document.getElementById("new-text");
 let quoteEl = document.getElementById("quotes");
 
-function getQuote(){
+function getQuote() {
     fetch("https://quote-garden.onrender.com/api/v3/quotes/random")
-    .then(function(response){
-        
-    return response.json()     
-    })
-    .then(function (quoteIndex) {
-        console.log(quoteIndex)
-        let displayQuote = quoteIndex.data[0].quoteText;
-        quoteEl.textContent = displayQuote;
-    
-    })
+        .then(function (response) {
+
+            return response.json()
+        })
+        .then(function (quoteIndex) {
+            console.log(quoteIndex)
+            let displayQuote = quoteIndex.data[0].quoteText;
+            quoteEl.textContent = displayQuote;
+
+        })
 }
 
-nextQuoteBtn.addEventListener("click", function (event){
+nextQuoteBtn.addEventListener("click", function (event) {
 
     getQuote();
     quoteEl.textContent = "";
-    
+
 }
 )
