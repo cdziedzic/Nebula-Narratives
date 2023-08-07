e7a660aa665e7505a7f632a032da678e9168628e
chris dziedzic <cdziedzic@gmail.com>
2023-08-07 13:14:39 -0400

    added functionality for calling nasa api and appending the picture to the image box for nasa

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,23 @@
-console.log("Cynthia")
-console.log('Chris Dziedzic')
-console.log("Jocelin");
+let nasaBox = document.getElementById('nasa-images')
+let newNasaButton = document.getElementById('new-image')
+
+function getNasa() { 
+    // calls nasa api for an image of the day and makes new img element
+    fetch (`https://api.nasa.gov/planetary/apod?api_key=p027dcXDBzAB1YTjEeaiAA2djDcIYC5joRZl66GR&count=1`)
+    .then(response => response.json())
+    .then (data => {
+    
+        let nasaImg = document.createElement('img')
+        nasaImg.src = data[0].hdurl
+        nasaBox.append(nasaImg)
+    })
+}
+
+// get new image when user clicks button
+newNasaButton.addEventListener('click', function (event) {
+    event.preventDefault()
+    getNasa();
+    nasaBox.textContent = ''
+})
+// commented load function out to save daily api calls
+// getNasa();
