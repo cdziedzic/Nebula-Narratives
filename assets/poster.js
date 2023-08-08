e087378827c81fe2d1a3f5ec6962ba73a00b3a8d
chris dziedzic <cdziedzic@gmail.com>
2023-08-08 11:16:16 -0400

    committing so i can switch branches

--- assets/poster.js
+++
@@ -1,9 +0,0 @@
-let buttonLinkEl = document.getElementById('new-image')
-
-
-addEventListener.buttonLinkEl('click', function (event) {
-    event.preventdefault()
-    window.location.href = "./index.html"
-}
-
-)
