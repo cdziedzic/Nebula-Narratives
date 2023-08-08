cc497216b7e112958224b61b4da98a3cd4fd64a7
chris dziedzic <cdziedzic@gmail.com>
2023-08-08 13:03:23 -0400

    created and styled modal box

--- assets/poster.js
+++ assets/poster.js
@@ -1,9 +1,9 @@
-let buttonLinkEl = document.getElementById('new-image')
+let buttonLinkEl = document.getElementById('save-image')
+let modal = document.getElementById('modal-box')
 
+buttonLinkEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    modal.style.display = "flex"
 
-addEventListener.buttonLinkEl('click', function (event) {
-    event.preventdefault()
-    window.location.href = "./index.html"
-}
+})
 
-)
