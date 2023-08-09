e167588b64b5b09ade11806202caa5b3c991fdf8
chris dziedzic <cdziedzic@gmail.com>
2023-08-09 11:13:57 -0400

    changed styling committing to pull

--- assets/poster.js
+++ assets/poster.js
@@ -1,34 +1,30 @@
-// display the quote on the poster (h1)
-let savedQuote = localStorage.getItem("quote");
-
-let displayQuote = document.getElementById("displayQuote")
-if(savedQuote){
-    displayQuote.textContent=savedQuote;
-}
-
-
-//display image on poster 
-
 let savedImageUrl = localStorage.getItem("imageUrl");
-
 let displayImg = document.getElementById("displayImg");
-
-
-if(savedImageUrl){
-
-    displayImg.src= savedImageUrl;
-
-}
-  
-
-//
-    
+let savedQuote = localStorage.getItem("quote");
+let displayQuote = document.getElementById("displayQuote")
 let buttonLinkEl = document.getElementById('title-image')
 let modal = document.getElementById('modal-box')
 let titleEl = document.getElementById('title')
 let submitButton = document.getElementById('submit')
 let inputValue = document.getElementById('input-box')
 
+
+
+// display the quote on the poster (h1)
+
+if(savedQuote){
+    displayQuote.textContent=savedQuote;
+
+}
+
+
+//display image on poster 
+
+if(savedImageUrl){
+ displayImg.src= savedImageUrl;
+
+}
+  
 //open the modal
 buttonLinkEl.addEventListener('click', function (event) {
     event.preventDefault();
